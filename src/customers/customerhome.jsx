d7adea1544e13243dce306f880684a014f868bc0
Chaeri-1 <alfsdo03@hufs.ac.kr>
2024-07-27 18:41:17 +0900

    search function update

--- src/customers/customerhome.jsx
+++ src/customers/customerhome.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import '../css/customerhome.css';
 import Searchbar from "./searchbar";
+import homeimg from "./img/customerhomeBanner1.jpg";
 
 export default function CustomerHome() {
   return(
-    <div>
+    <div className="customer-home">
       <div className="header">
         <h2>배지</h2>
         <div>
           <span>내정보</span>
           <span>로그아웃</span>
         </div>
+      </div>
+      <div className="homebanner">
+        <img src={homeimg}/>
       </div>
       <div>
         <h1>어떤 지역의 가게를 찾으시나요?</h1>
--- src/customers/searchbar.jsx
+++ src/customers/searchbar.jsx
@@ -1,9 +1,16 @@
 import React, {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Searchbar() {
   //useState로 값 관리
+  //const [isFocus, setIsFocus] = useState(false);
   const [keyword, setKeyword] = useState("");
-  const [filteredContent, setFileredContent] = useState();
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    navigate(`customer/search/${encodeURIComponent(keyword)}`)//특수문자 포함한 URL인코딩?
+  }
 
   //고객이 글씨를 쓸때마다 반영되게
   const onChange = (e) => {
@@ -15,10 +22,12 @@ export default function Searchbar() {
   return(
     <div>
       <input
-        placeholder="지역..."
+        placeholder="ex.이문동"
         value={keyword}
         onChange={onChange}
       />
+      <button onClick={handleSearch}>검색</button>
     </div>
   )
 };
+
