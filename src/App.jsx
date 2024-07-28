31be4e239b23326cc6cbdc10b46d86c6a8ea4470
igbinni143 <bin143@naver.com>
2024-07-29 02:03:45 +0900

    nicknametostorname

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import FormLogin from "./pages/formlogin.jsx";
 import CustomerHome from "./customers/customerhome";
-import SellerPage from "./sellers/sellerpage.jsx";
+import SellermainPage from "./sellers/sellermainpage.jsx";
 import Sellershop from "./sellers/sellershop.jsx";
 import SearchList from "./customers/searchlist.jsx";
 import InitialSearch from "./customers/initialsearch.jsx";
@@ -15,10 +15,12 @@ function App() {
 				<Route path='/' element={<Homepage />} />
 				<Route path='/login' element={<FormLogin />} />
 				<Route path='/customer' element={<CustomerHome />}>
-					<Route index={true} element={<InitialSearch/>}/>
-					<Route path='search/:place' element={<SearchList/>}/>
+					<Route index={true} element={<InitialSearch />} />
+					<Route path='search/:place' element={<SearchList />} />
 				</Route>
 				<Route path='/seller' element={<SellerPage />} />
+				<Route path='/customer' element={<CustomerHome />} />
+				{/* <Route path='/seller' element={<SellermainPage />} /> */}
 				<Route path='/sellershop' element={<Sellershop />} />
 			</Routes>
 		</Router>
---
+++ src/sellers/sellermainpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sellernav from "./sellernav.jsx";
+import "../sellers/sellercss/sellerbanner.css";
+
+function SellermainPage() {
+	return (
+		<>
+			<Sellernav />
+		</>
+	);
+}
+
+export default SellermainPage;
