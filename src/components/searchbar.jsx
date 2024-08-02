f7c63133dfe2a6351be1451f6fb61b694f442c58
igbinni143 <bin143@naver.com>
2024-08-02 18:29:40 +0900

    customer

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,72 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const InputWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 30px;
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 30px;
+`;
 
 const Input = styled.input`
-  width: 90vw;
-  max-width: 500px;
-  height: 30px;
-  &:focus {
-    border-color: #B0D9B6;
-    border-width: 1.5px;
-    outline: none;
-  }
-`
+	width: 90vw;
+	max-width: 500px;
+	height: 30px;
+	&:focus {
+		border-color: #b0d9b6;
+		border-width: 1.5px;
+		outline: none;
+	}
+`;
 const Button = styled.button`
-  display: inline-flex;
-  align-items: center;
-  background-color: #d2d2d2;
-  margin-left: 20px;
-  height: 43px;
-  border-radius: 5px;
-  font-size: 15px;
-  
-  &:hover {
-    transform: scale(1.1);
-    background-color: #B0D9B6;
-  }
-`
+	display: inline-flex;
+	align-items: center;
+	background-color: #d2d2d2;
+	margin-left: 20px;
+	height: 43px;
+	border-radius: 5px;
+	font-size: 15px;
+	&:hover {
+		transform: scale(1.1);
+		background-color: #b0d9b6;
+	}
+`;
 
 export default function Searchbar() {
-  //useState로 값 관리
-  //const [isFocus, setIsFocus] = useState(false);
-  const [keyword, setKeyword] = useState("");
-  const navigate = useNavigate();
+	const [keyword, setKeyword] = useState("");
+	const navigate = useNavigate();
 
-  //검색한 값이 없으면 이동 안되게 만들기!
-  const handleSearch = () => {
-    navigate(`search/${encodeURIComponent(keyword)}`, {replace: true})//특수문자 포함한 URL인코딩?
-  }
+	const handleSearch = (e) => {
+		if (e) e.preventDefault();
+		if (keyword.trim()) {
+			navigate(`/customer/search/${encodeURIComponent(keyword.trim())}`);
+		}
+	};
 
-  //고객이 글씨를 쓸때마다 반영되게
-  const onChange = (e) => {
-    setKeyword(e.target.value);
-  }
+	const onChange = (e) => {
+		setKeyword(e.target.value);
+	};
 
-  const activeEnter = (e) => {
-    if(e.key === "Enter") {
-      handleSearch();
-    }
-  }
+	const activeEnter = (e) => {
+		if (e.key === "Enter") {
+			handleSearch();
+		}
+	};
 
-  //console.log(setKeyword)
-  //const filterContent = 
-  return(
-    <InputWrapper>
-      <Input
-        placeholder="ex.이문동"
-        value={keyword}
-        onChange={onChange}
-        onKeyDown={(e) => activeEnter(e)}
-      />
-      <Button onClick={handleSearch}>검색</Button>
-    </InputWrapper>
-  )
-};
+	return (
+		<InputWrapper>
+			<Input placeholder='ex.이문동' value={keyword} onChange={onChange} onKeyDown={activeEnter} />
+			<Button onClick={handleSearch}>검색</Button>
+		</InputWrapper>
+	);
+}
